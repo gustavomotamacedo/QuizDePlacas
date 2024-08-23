b852ac2737a337fe134792af49f7519e64cee149
Gustavo Macedo <mota.macedo05@gmail.com>
2024-08-23 00:18:43 -0300

    feat: quiz de placas

---
+++ app/src/main/java/com/gustavomacedo/quizdeplacas/PlacaCinco.java
@@ -0,0 +1,33 @@
+package com.gustavomacedo.quizdeplacas;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class PlacaCinco extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_placa_cinco);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Button btnEnviar = findViewById(R.id.btnEnviar);
+
+        btnEnviar.setOnClickListener(v -> {
+            finish();
+            Toast.makeText(getApplicationContext(), "App finalizado.", Toast.LENGTH_SHORT).show();
+        });
+    }
+}
---
+++ app/src/main/java/com/gustavomacedo/quizdeplacas/PlacaQuatro.java
@@ -0,0 +1,34 @@
+package com.gustavomacedo.quizdeplacas;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class PlacaQuatro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_placa_quatro);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Button btnEnviar = findViewById(R.id.btnEnviar);
+
+        btnEnviar.setOnClickListener(v -> {
+            Intent in = new Intent(getApplicationContext(), PlacaCinco.class);
+            finish();
+            startActivity(in);
+        });
+    }
+}
